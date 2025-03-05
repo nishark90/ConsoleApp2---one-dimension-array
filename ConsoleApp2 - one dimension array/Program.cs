4eb223ffdfcb1e6a762e623fed4a579a2a039f70
Nisha R K <55803996+nishark90@users.noreply.github.com>
2025-03-05 21:16:06 +0530

    Add project files.

---
+++ ConsoleApp2 - one dimension array/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2___one_dimension_array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {// one dimension array//
+            Console.WriteLine("Enter the size of the array");
+            int size = Convert.ToInt32(Console.ReadLine());
+            int[] arr = new int[size];
+            Console.WriteLine("Enter the elements of the array");
+            int i;
+            for (i = 0; i < size; i++)
+            {
+                arr[i] = Convert.ToInt32(Console.ReadLine());
+            }
+            Console.WriteLine("The elements of the array are");
+            for (i = 0; i < size; i++)
+            {
+                Console.WriteLine(arr[i]);
+            }
+        }
+    }
+}
